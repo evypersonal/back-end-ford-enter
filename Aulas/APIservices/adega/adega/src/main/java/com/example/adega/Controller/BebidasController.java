228524837592a61076ba56b5035002e1b63d0b8d
Evellyn de Oliveira <108280573+evypersonal@users.noreply.github.com>
2025-06-10 19:07:20 +0000

    feat: subindo projeto CRUD 'adega'

--- Aulas/APIservices/adega/adega/src/main/java/com/example/adega/Controller/BebidasController.java
+++ Aulas/APIservices/adega/adega/src/main/java/com/example/adega/Controller/BebidasController.java
@@ -3,10 +3,15 @@ package com.example.adega.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,4 +41,18 @@ public class BebidasController {
 		return ResponseEntity.ok(repository.findById(id));
 	}
 	
+	@PostMapping
+	public ResponseEntity<Bebidas> post(@RequestBody Bebidas bebidas){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(bebidas));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Bebidas> put(@RequestBody Bebidas bebidas){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(bebidas));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
--- Aulas/APIservices/adega/adega/src/main/java/com/example/adega/Controller/TipoController.java
+++ Aulas/APIservices/adega/adega/src/main/java/com/example/adega/Controller/TipoController.java
@@ -3,15 +3,21 @@ package com.example.adega.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.adega.Model.Tipo;
 import com.example.adega.Repository.TipoRepository;
+
 
 @RestController
 
@@ -36,5 +42,20 @@ public class TipoController {
 	@GetMapping("id/{id}")
 	public ResponseEntity<List<Tipo>> getById(@PathVariable long id){
 		return ResponseEntity.ok(repository.findById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Tipo> post(@RequestBody Tipo tipo){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tipo));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Tipo> put(@RequestBody Tipo tipo){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tipo));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
